6ddca939bd7faa0ef9e4cd86daaf1298f51eed14
jiangyu <945803392@qq.com>
2016-03-02 20:47:37 +0800

    【更新】更新一下字符串常量的值

--- platform/src/main/java/com/jhon/yu/code/generator/platform/constants/StrConstant.java
+++ platform/src/main/java/com/jhon/yu/code/generator/platform/constants/StrConstant.java
@@ -11,8 +11,10 @@ public interface StrConstant
     /** 默认的加密明文 **/
     String DEFAULT_PLAIN_TEXT="platform@Version20160108";
     
+    
     /** session作用域中的变量名 **/
     String SESSION_VAR_SHOP_ID = "lg";
+    
 
     /** session作用域中的变量名 **/
     String SESSION_VAR_GLOBAL = "global";
